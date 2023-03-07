a0819a39c28376325d298c41584264d293e713c2
KIM DAEHO <mrkim9427@naver.com>
2023-03-07 17:41:04 +0900

    비즈니스 컴포넌트 실습 1

---
+++ src/main/resources/sql/SQL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE b_board (
+	seq NUMBER(5) PRIMARY KEY,
+    title VARCHAR2(200),
+    writer VARCHAR2(20),
+    content VARCHAR2(2000),
+    regdate DATE DEFAULT SYSDATE,
+    cnt NUMBER(5) DEFAULT 0
+);
+
+SELECT * FROM b_board;
