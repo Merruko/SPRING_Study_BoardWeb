fe70fb812a48c07d7b3f8cb02a848175cc91812a
KIM DAEHO <mrkim9427@naver.com>
2023-04-08 20:51:02 +0900

    Spring MVC 어노테이션 적용

--- src/main/java/com/springbook/view/user/LogoutController.java
+++ src/main/java/com/springbook/view/user/LogoutController.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-public class LogoutController implements Controller {
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+@Controller
+public class LogoutController {
+	
+	@RequestMapping("/logout.do")
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("로그아웃 처리");
 		
 		// 1. 브라우저와 연결된 세션 객체를 강제 종료 
@@ -17,8 +19,6 @@ public class LogoutController implements Controller {
 		session.invalidate();
 
 		// 2. 세션 종료 후, 메인 화면으로 이동
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:login.jsp");
-		return mav;
+		return "login.jsp";
 	}
 }
